5a1bcb501b6064e97c12afd4aa284a6a9d2b8c00
clerdee <clerdeecruz@gmail.com>
2026-07-02 01:23:55 +0800

    super admin

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getAllUsers, approveAdmin, updateUser } = require('../controllers/adminController');
 const authMiddleware = require('../middleware/authMiddleware');
-const { adminMiddleware, superAdminMiddleware } = require('../middleware/roleMiddleware');
+const { adminMiddleware, superAdminMiddleware } = require('../middleware/adminMiddleware');
 
 router.get('/users', authMiddleware, adminMiddleware, getAllUsers);
 router.put('/users/:id/approve', authMiddleware, superAdminMiddleware, approveAdmin);
